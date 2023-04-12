713a6190f32f1b6f6ef4d6e3ff85d0308c25a89f
BackoKillerSVK <MBBaco@gmail.com>
2023-04-12 19:38:20 +0200

    Frontend

--- ClientApp/src/pages/HomePage.jsx
+++ ClientApp/src/pages/HomePage.jsx
@@ -1,5 +1,4 @@
 ﻿import React from "react";
-import { Link } from "react-router-dom";
 import { Col, Container, Row } from "reactstrap";
 import CardComponent from "../components/main/card";
 import "./HomePage.css"
