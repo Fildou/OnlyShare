3a9f08e2a68cf1f90ffacd68b2597d8142cea818
BackoKillerSVK <MBBaco@gmail.com>
2023-04-13 19:12:41 +0200

    SS

--- ClientApp/src/pages/CreateQuestion.jsx
+++ ClientApp/src/pages/CreateQuestion.jsx
@@ -3,45 +3,63 @@ import axios from "axios";
 import { Button, Form, FormGroup, Input, Label, FormText } from "reactstrap";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import "./CreateQuestion.css";
 import { useNavigate } from "react-router-dom";
 
 const CreateQuestion = () => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const [titleError, setTitleError] = useState("");
+  const [descriptionError, setDescriptionError] = useState("");
   const navigate = useNavigate();
 
   const handleSave = async (e) => {
     e.preventDefault();
 
-    try {
-      const token = localStorage.getItem("token");
-      const config = {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      };
+    // validate title and description
+    setTitleError(validateField(title));
+    setDescriptionError(validateField(description));
 
-      const response = await axios.post(
-        "/api/questions/AddQuestion",
-        { title, description },
-        config
-      );
+    if (title && description) {
+      try {
+        const token = localStorage.getItem("token");
+        const config = {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        };
 
-      toast.success("Question created successfully!");
-      setTimeout(() => {
-        navigate("/");
-      }, 3000);
-    } catch (error) {
-      console.error(error);
-      toast.error("An error occurred while creating the question.");
+        const response = await axios.post(
+          "/api/questions/AddQuestion",
+          { title, description },
+          config
+        );
+
+        toast.success("Question created successfully!");
+        setTimeout(() => {
+          navigate("/");
+        }, 3000);
+      } catch (error) {
+        console.error(error);
+        toast.error("An error occurred while creating the question.");
+      }
+    }
+  };
+
+  const validateField = (value) => {
+    if (!value) {
+      return "This field is required.";
+    } else {
+      return "";
     }
   };
 
   return (
-    <div className="container">
+    <div  className="container form-create">
       <h1>Create question</h1>
-      <Form onSubmit={handleSave}>
+      <Form  onSubmit={handleSave}>
+
         <FormGroup>
           <Label for="title">Title</Label>
           <Input
@@ -52,8 +70,10 @@ const CreateQuestion = () => {
             value={title}
             onChange={(e) => setTitle(e.target.value)}
           />
+          {titleError && <div className="error">{titleError}</div>}
           <FormText>Please enter a title for your question</FormText>
         </FormGroup>
+
         <FormGroup>
           <Label for="description">Description</Label>
           <Input
@@ -64,10 +84,14 @@ const CreateQuestion = () => {
             value={description}
             onChange={(e) => setDescription(e.target.value)}
           />
+          {descriptionError && (
+            <div className="error">{descriptionError}</div>
+          )}
           <FormText>
             Please enter a detailed description of your question
           </FormText>
         </FormGroup>
+
         <Button type="submit" className="">
           Save
         </Button>
@@ -78,3 +102,6 @@ const CreateQuestion = () => {
 };
 
 export default CreateQuestion;
+
+
+
