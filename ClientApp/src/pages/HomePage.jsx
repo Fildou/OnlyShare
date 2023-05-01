ed2e5d449e68fb24f2612562bb9ef1b7de183373
DaliborDuris <87717913+Dalibor-Duris-code@users.noreply.github.com>
2023-05-01 15:17:33 +0200

    searchbar fix

--- ClientApp/src/pages/HomePage.jsx
+++ ClientApp/src/pages/HomePage.jsx
@@ -10,37 +10,40 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 const HomePage = () => {
-  const [questions, setQuestions] = useState([]);
-  const { isLoggedIn, toggleLogin, user } = useAuth();
-  const navigate = useNavigate();
-  const posts = [
-  
-  ];
+    const [questions, setQuestions] = useState([]);
+    const { isLoggedIn, toggleLogin, user } = useAuth();
+    const navigate = useNavigate();
+    const posts = [];
+    const [searchQuery, setSearchQuery] = useState("");
 
-  
-  useEffect(() => {
-    const fetchQuestions = async () => {
-      try {
-        const response = await axios.get("/api/questions/getquestions");
-        setQuestions(response.data);
-      } catch (error) {
-        console.error("Error fetching questions:", error);
-      }
+    useEffect(() => {
+        const fetchQuestions = async () => {
+            try {
+                const response = await axios.get("/api/questions/getquestions");
+                setQuestions(response.data);
+            } catch (error) {
+                console.error("Error fetching questions:", error);
+            }
+        };
+
+        fetchQuestions();
+    }, []);
+
+    const formatDate = (dateString) => {
+        const date = new Date(dateString);
+        return date.toLocaleDateString("en-US", {
+            year: "numeric",
+            month: "short",
+            day: "numeric",
+        });
     };
 
-    fetchQuestions();
-  }, []);
-
-  const formatDate = (dateString) => {
-    const date = new Date(dateString);
-    return date.toLocaleDateString("en-US", {
-        year: "numeric",
-        month: "short",
-        day: "numeric",
+    const allPosts = [...questions, ...posts].sort((a, b) => b.id - a.id).filter((post) => {
+        return (
+            post.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            post.description.toLowerCase().includes(searchQuery.toLowerCase())
+        );
     });
-};
-
-  const allPosts = [...questions, ...posts].sort((a, b) => b.id - a.id);
 
     const handleCreateQuestion = () => {
         if (isLoggedIn) {
@@ -49,38 +52,43 @@ const HomePage = () => {
             navigate("/login");
         }
     };
-  
-  return (
-      <Container>
-          <input className="form-check-input w-100 h-25 mb-5" placeholder="Search.."/>
-          <div className="d-flex  align-items-center justify-content-between mb-5">
-              <h1 className="post-text">Questions</h1>
-              <button className="btn btn-primary" onClick={handleCreateQuestion}>Create <FontAwesomeIcon icon={faPlus}/></button>
-          </div>
-          <Row>
-              {allPosts.map((post) => (
-                  <Col key={post.id} md="12" className="mb-4">
-                      <CardComponent
-                          title={post.title}
-                          username={
-                              post.createdByUserId ? (
-                                  <Link to={`/profile/${post.createdByUserId}`}>
-                                      {post.createdByUserName}
-                                  </Link>
-                              ) : (
-                                  <span>{post.createdByUserName}</span>
-                              )
-                          }
-                          text={post.text || post.description}
-                          date={formatDate(post.date || post.createdAt)}
-                          postId={post.id}
-                      />
-                  </Col>
-              ))}
-          </Row>
-      </Container>
-  );
-};
 
+    const handleSearchInputChange = (event) => {
+        setSearchQuery(event.target.value);
+    };
+
+    return (
+        <Container>
+            <input className="form-check-input w-100 h-25 mb-5" placeholder="Search.." value={searchQuery} onChange={handleSearchInputChange} />
+            <div className="d-flex  align-items-center justify-content-between mb-5">
+                <h1 className="post-text">Questions</h1>
+                <button className="btn btn-primary" onClick={handleCreateQuestion}>
+                    Create <FontAwesomeIcon icon={faPlus}/>
+                </button>
+            </div>
+            <Row>
+                {allPosts.map((post) => (
+                    <Col key={post.id} md="12" className="mb-4">
+                        <CardComponent
+                            title={post.title}
+                            username={
+                                post.createdByUserId ? (
+                                    <Link to={`/profile/${post.createdByUserId}`}>
+                                        {post.createdByUserName}
+                                    </Link>
+                                ) : (
+                                    <span>{post.createdByUserName}</span>
+                                )
+                            }
+                            text={post.text || post.description}
+                            date={formatDate(post.date || post.createdAt)}
+                            postId={post.id}
+                        />
+                    </Col>
+                ))}
+            </Row>
+        </Container>
+    );
+};
 
 export default HomePage;
